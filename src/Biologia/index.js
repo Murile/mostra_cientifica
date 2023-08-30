12f71dd5df969bb08a01bdfedc4624fe583ea367
Rodrigo Olivera Anjos <rodrigo.anjos0806@gmail.com>
2023-08-30 09:21:34 -0300

    commiasadadad

--- src/Biologia/index.js
+++ src/Biologia/index.js
@@ -1,8 +1,22 @@
 import './index.scss'
-import { Container, row, Col } from "reactscript";
+import { Container, Row,Col } from "reactscript";
 
 function Bio() {
     return (
-        <div></div>
+        <div className='main'>
+            <header>
+                <Container>
+                    <Row>
+                        <Col lg={12}>
+                            <div className='wrapper-img'>
+                                <img src="../../public/img/logo-mostra.png" alt="" />
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>   
+            </header>
+        </div>
     );
 }
+
+export default Bio;
--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Home from './Home'
+import Biologia from './Biologia'
 
 export default function Rotas() {
     return(
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/bio' element={<Biologia />} />
             </Routes>
         </BrowserRouter>
     )
