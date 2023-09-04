9fb513dc964e12f55b22e2c879da1d323cfff0b4
Rodrigo Olivera Anjos <rodrigo.anjos0806@gmail.com>
2023-09-03 22:19:05 -0300

    Trabalha nunca ne?

--- src/Biologia/index.js
+++ src/Biologia/index.js
@@ -19,13 +19,61 @@ function Bio() {
                 <Container>
                     <Row>
                         <Col lg={12}>
-                            <div className='wrapper-title'>
-                                <h1>Experiências</h1>
+                            <div className='wrapper-superior'>
+                                <div className='wrapper-title'>
+                                    <h1>Experiências</h1>
+                                </div>
+                                <div className='wrapper-btn'>
+                                    <a href="/"><img src="../../img/arrow-left.svg" alt=""/>Voltar</a>
+                                </div>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col lg={3}>
+                            <div className='card-conteudo'>
+                                <img src="#" alt="" />
+                            </div>
+                        </Col>
+                        <Col lg={3}>
+                            <div className='card-conteudo'>
+                                <img src="#" alt="" />
+                            </div>
+                        </Col>
+                        <Col lg={3}>
+                            <div className='card-conteudo'>
+                                <img src="#" alt="" />
+                            </div>
+                        </Col>
+                        <Col lg={3}>
+                            <div className='card-conteudo'>
+                                <img src="#" alt="" />
+                            </div>
+                        </Col>
+                        <Col lg={3}>
+                            <div className='card-conteudo'>
+                                <img src="#" alt="" />
+                            </div>
+                        </Col>
+                        <Col lg={3}>
+                            <div className='card-conteudo'>
+                                <img src="#" alt="" />
                             </div>
                         </Col>
                     </Row>
                 </Container>
             </section>
+            <footer>
+                <Container>
+                    <Row>
+                        <Col lg={12}>
+                            <div className='wrapper-rodape d-flex justify-content-center'>
+                                <p><span>Desenvolvido por:</span> Pedro Reis, Rodrigo Anjos, Murilo Barbosa</p>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
         </main>
     );
 }
