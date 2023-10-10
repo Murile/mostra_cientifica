d1438b31d8c95fedb68e2c71b67aba116405d556
Rodrigo Oliveira Anjos <rodrigo.anjos0806@gmail.com>
2023-10-09 21:15:43 -0300

    iajdoiajdoia

--- src/Biologia/index.js
+++ src/Biologia/index.js
@@ -2,13 +2,16 @@ import './index.scss'
 import { Container, Row,Col } from "reactstrap";
 
 function Bio() {
+    const popup = document.querySelector('#popups');
 
-    async function Popup() {
-        const elemento = document.querySelector('.card-conteudo');
-        if (elemento.classList.contains('classe') == false) {
-            elemento.classList.add('classe');
+    async function Popup1() {
+        const c = document.querySelector('#popup-1');
+        if (c.classList.contains('#popup-1') == false) {
+            c.classList.add('active');
+            popup.classList.add('active');
         } else {
-            elemento.classList.remove('classe');
+            c.classList.remove('active');
+            popup.classList.remove('active');
         }
     }
 
@@ -41,37 +44,67 @@ function Bio() {
                     </Row>
                     <Row>
                         <Col lg={3}>
-                            <div onClick={Popup} className='card-conteudo' data-id='1'>
+                            <div onClick={Popup1} className='card-conteudo' id='1'>
                                 <img src="#" alt="" />
                             </div>
                         </Col>
                         <Col lg={3}>
-                            <div className='card-conteudo' data-id='2'>
+                            <div className='card-conteudo' id='2'>
                                 <img src="#" alt="" />
                             </div>
                         </Col>
                         <Col lg={3}>
-                            <div className='card-conteudo' data-id='3'>
+                            <div className='card-conteudo' id='3'>
                                 <img src="#" alt="" />
                             </div>
                         </Col>
                         <Col lg={3}>
-                            <div className='card-conteudo' data-id='4'>
+                            <div className='card-conteudo' id='4'>
                                 <img src="#" alt="" />
                             </div>
                         </Col>
                         <Col lg={3}>
-                            <div className='card-conteudo' data-id='5'>
+                            <div className='card-conteudo' id='5'>
                                 <img src="#" alt="" />
                             </div>
                         </Col>
                         <Col lg={3}>
-                            <div className='card-conteudo' data-id='6'>
+                            <div className='card-conteudo' id='6'>
                                 <img src="#" alt="" />
                             </div>
                         </Col>
                     </Row>
                 </Container>
+            </section>
+            <section id='popups'>
+                <div id="popup-1" className='popup-content'>
+                    <div onClick={Popup1} className='wrapper-close'>
+                        <a href="#">
+                            <img src="../../img/close.svg" alt="" />
+                        </a>
+                    </div>
+                    <div className='wrapper-img'>
+                        <img src="#" alt="" />
+                    </div>
+                    <div className='wrapper-descritivo'>
+                        <h1>Experiência</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non curabitur gravida arcu ac tortor dignissim convallis aenean et. Ultricies leo integer malesuada nunc vel. Est ullamcorper eget nulla facilisi. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Morbi quis commodo odio aenean. Phasellus vestibulum lorem sed risus ultricies tristique.</p>
+                    </div>
+                </div>
+                <div id="popup-2" className='popup-content'>
+                    <div className='wrapper-close'>
+                        <a href="#">
+                            <img src="../../img/close.svg" alt="" />
+                        </a>
+                    </div>
+                    <div className='wrapper-img'>
+                        <img src="#" alt="" />
+                    </div>
+                    <div className='wrapper-descritivo'>
+                        <h1>Experiência</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non curabitur gravida arcu ac tortor dignissim convallis aenean et. Ultricies leo integer malesuada nunc vel. Est ullamcorper eget nulla facilisi. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit. Morbi quis commodo odio aenean. Phasellus vestibulum lorem sed risus ultricies tristique.</p>
+                    </div>
+                </div>
             </section>
             <footer>
                 <Container>
